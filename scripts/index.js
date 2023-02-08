88ea69662139f6fa0b3089e4d2f2cdc0244df6bb
Pritam Borole <borolepritam@gmail.com>
2023-02-09 02:00:34 +0530

    Added Slideshow

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-
+// drop down for categories
 function showDiv() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -15,3 +15,24 @@ window.onclick = function(event) {
       }
     }
   }
+
+// automatic slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+  console.log("here")
+  showSlides(slideIndex += n);
+}
